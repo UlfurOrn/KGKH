fa7541a93ae811a5e051772a499f4c1281d70a2e
Ulfur_Orn <ulfurinn@gmail.com>
2020-03-04 10:47:00 +0000

    Created Tests for FizzBuzz function

---
+++ test_sample.py
@@ -0,0 +1,16 @@
+from sample import fizzbuzz
+
+def test_return_fizzbuzz():
+    assert fizzbuzz(15) == "FizzBuzz"
+
+
+def test_return_buzz():
+    assert fizzbuzz(5) == "Buzz"
+
+
+def test_return_fizz():
+    assert fizzbuzz(3) == "Fizz"
+
+
+def test_return_number():
+    assert fizzbuzz(2) == 2
